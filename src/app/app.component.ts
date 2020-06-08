46ab917feed617861dfc550534d48870fe6166f2
Azad Kavian <azad.kavian@gmail.com>
2020-06-08 23:21:54 +0430

    added tawkto service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { LocaleService, TawkToService } from '@core/services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Invest';
+  constructor(
+    private localeService: LocaleService,
+    private tawkToService: TawkToService
+  ) {}
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RESTModule } from '@core/modules/rest.module';
-import { AuthService, StorageService, TraderService } from '@core/services';
+import {
+  AuthService,
+  StorageService,
+  TraderService,
+  TawkToService,
+} from '@core/services';
 
 @NgModule({
   declarations: [],
   imports: [RESTModule],
-  providers: [AuthService, StorageService, TraderService],
+  providers: [AuthService, StorageService, TraderService, TawkToService],
   bootstrap: [],
 })
 export class CoreModule {}
